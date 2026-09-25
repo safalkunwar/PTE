06ddaa9b0bb4f00453acbffa4b669e44499c4ef0
Safal Kunwar <129306053+safalkunwar@users.noreply.github.com>
2026-09-25 19:35:21 +0545

    Fix formatting issue in useMobile hook

--- client/src/hooks/useMobile.tsx
+++ client/src/hooks/useMobile.tsx
@@ -5,7 +5,7 @@ const MOBILE_BREAKPOINT = 768;
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
     undefined
-  );//testing 
+  );
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
